b92979c1e045ba722425a7621088bd17822e5f16
VitorGreff <vitorgreffdias@gmail.com>
2023-10-21 16:05:34 -0300

    almost there

--- hbf.py
+++ hbf.py
@@ -18,20 +18,22 @@ def plot_results(before, after):
 
     plt.show()
 
-def high_boost_filter(imagem, matrix):
+def high_boost_filter(imagem, kernel, peso):
     # borra a imagem
-    imagem_borrada = cv2.GaussianBlur(imagem, matrix, 0)
+    imagem_borrada = cv2.GaussianBlur(imagem.astype(np.float32), kernel, 1)
+    print(imagem_borrada)
     
     # a mascara é definida pela diferença entra a imagemm borrada e a original
     mask = np.subtract(imagem, imagem_borrada)
 
     # o resultado é a soma da máscara com a imagem borrada
-    resultado = np.add(imagem, mask)
-    plot_results(imagem, resultado)
+    resultado = np.add(imagem, np.multiply(mask, peso))
+    plot_results(imagem, resultado.astype(np.uint16))
 
 
 if __name__ == "__main__":
-    # imagem= cv2.imread("lena_gray_512.tif")
-    imagem= cv2.imread("imagens/pirate.tif")
+    # imagem= cv2.imread("imagens/lena_gray_512.tif")
+    # imagem= cv2.imread("imagens/pirate.tif")
+    imagem= cv2.imread("imagens/woman_darkhair.tif")
     # imagem= cv2.imread("house.tif")s
-    high_boost_filter(imagem, (7,7))
+    high_boost_filter(imagem, (7,7), 1)
