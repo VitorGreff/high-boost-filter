ebd2638e7bd1ecc0ba56ac8bc43601628d7f7d28
VitorGreff <vitorgreffdias@gmail.com>
2023-10-22 13:03:13 -0300

    clipping images

--- hbf.py
+++ hbf.py
@@ -19,21 +19,21 @@ def plot_results(before, after):
     plt.show()
 
 def high_boost_filter(imagem, kernel, peso):
+    imagem = imagem.astype(np.float32)
     # borra a imagem
-    imagem_borrada = cv2.GaussianBlur(imagem.astype(np.float32), kernel, 1)
-    print(imagem_borrada)
-    
+    imagem_borrada = cv2.GaussianBlur(imagem, kernel, 1)
+    # plot_results(imagem, imagem_borrada.astype(np.uint16))
     # a mascara é definida pela diferença entra a imagemm borrada e a original
     mask = np.subtract(imagem, imagem_borrada)
-
-    # o resultado é a soma da máscara com a imagem borrada
+    # o resultado é a soma da máscara com a imagem borrada multiplicada pelo peso
     resultado = np.add(imagem, np.multiply(mask, peso))
-    plot_results(imagem, resultado.astype(np.uint16))
+    plot_results(np.clip(imagem, 0, 255).astype(np.uint8), np.clip(resultado, 0, 255).astype(np.uint8))
 
 
 if __name__ == "__main__":
     # imagem= cv2.imread("imagens/lena_gray_512.tif")
-    # imagem= cv2.imread("imagens/pirate.tif")
     imagem= cv2.imread("imagens/woman_darkhair.tif")
+    # imagem= cv2.imread("imagens/jetplane.tif")
+    # imagem= cv2.imread("imagens/pirate.tif")
     # imagem= cv2.imread("house.tif")s
-    high_boost_filter(imagem, (7,7), 1)
+    high_boost_filter(imagem, (7,7), 3.2)
